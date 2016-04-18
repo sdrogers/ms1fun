44634e0cd45797687d99ecabe3576352690dfd0d
Simon Rogers <simon.rogers@glasgow.ac.uk>
2016-04-18 15:53:17 +0100

    tried the voting scheme on the beer data

---
+++ conversion_scripts/extract_csv.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -jar alignment.jar -d mzmatch.experimental.PeakMLToCsv /Users/simon/Downloads/Beer3_mzXML_mzML_files/PositiveMode/Beer3Full/ -rtWindow 10
---
+++ conversion_scripts/extract_signals.sh
@@ -0,0 +1,2 @@
+
+java -ea -cp .:/Library/Frameworks/R.framework/Versions/Current/Resources/library/mzmatch.R/java/mzmatch_2.0.jar GetSignals /Users/simon/Downloads/Beer3_mzXML_mzML_files/PositiveMode/Beer3Full/Beer_3_Full1.peakml /Users/simon/Downloads/Beer3_mzXML_mzML_files/PositiveMode/Beer3Full/csv/Beer_3_Full1.signal
