b17565a67b83389ea5ae4db72ce3b29f2304434a
vilisimo <vilisimo@gmail.com>
2016-07-06 17:28:06 +0100

    Mostly cosmetic changes to the code

--- sv_algorithm/sv_algorithm.py
+++ sv_algorithm/sv_algorithm.py
@@ -1,13 +1,12 @@
 # Questions:
-# 1. Total vote is very low (0.32967032967 compared to 6.765 in ..._by_vote.txt). Changes to transformations' files?
-# 2. Calculation of the mass in PeakGroup - is it needed?
-# 3. Let's say we ignore isotopes. But the results don't match. Is it a problem? Or have we just avoided false groups?
-# 4. Testing: both for thesis & to make sure algorithm is bug-free. What's the best approach? One part would be
+# 1. Calculation of the mass in PeakGroup - is it needed?
+# 2. Let's say we ignore isotopes. But the results don't match. Is it a problem? Or have we just avoided false groups?
+# 3. Testing: both for thesis & to make sure algorithm is bug-free. What's the best approach? One part would be
 # profiling. Others - unit tests?
-# 5. [M-C2H2]+2H[2C13] parent is [M-C2H2]+2H[C13]? What about [M-C2H2]+H[C13]? At the moment code relies on former.
+# 4. [M-C2H2]+2H[2C13] parent is [M-C2H2]+2H[C13]? What about [M-C2H2]+H[C13]? At the moment code relies on former.
 
 import os, sys
-sys.path.insert(0, os.path.normpath(os.getcwd() + os.sep + os.pardir) + '/code/')
+sys.path.insert(0, '../code/')
 from corr_cluster import Peak
 
 
@@ -26,27 +25,28 @@ class PeakGroup(object):
             elif peak is p and transformation.vote < t.vote:
                 return
 
-        # This is reached either when there is no second peak, or the vote of the same peak's transformation is higher.
+        # Reached either when there is no second peak, or the vote of the same peak's transformation is higher.
         self.members.append((peak, transformation, transformation.transform(peak)))
         self.recalculate()
 
-    # Recalculates votes, intensity, mass. Used when there are changes to the group.
+    # Recalculates votes, intensity, mass. Used when members are added/removed from a group.
     def recalculate(self):
+        total_intensity = 0.0   # Total intensity of all peaks in the group.
+        self.M = 0.0            # Total mass of peaks in the group.
+        self.vote = 0.0         # Total vote of all transformations in the group.
+
         # In case group does not have any members left (e.g. peak without a parent was removed leaving it empty)
         if len(self.members) <= 0:
             return
 
-        # Calculates mass of a group. Adapted from voter.py.
-        total_intensity = 0.0    # Total intensity of all peaks in the group.
-        self.M = 0.0             # To reset mass each time the peak is added.
-        self.vote = 0.0
+        # Calculates mass and vote of a group. Adapted from voter.py.
         for member in self.members:
             self.vote += member[1].vote                 # total vote += transformation.vote
             self.M += member[0].intensity * member[2]   # mass = peak's intensity * transformed peak
             total_intensity += member[0].intensity      # total intensity += peak intensity
         self.M /= total_intensity
 
-    # For printing in terminal. No other purpose, can be deleted later on to make the code tidier.
+    # For printing in command line. No other purpose, can be deleted later on to make the code tidier.
     def __str__(self):
         representation = "\nTotal vote: {} Mass: {}".format(self.vote, self.M)
         for peak, transformation, transformed_mass in sorted(self.members, key=lambda x: x[1].vote, reverse=True):
@@ -55,7 +55,7 @@ class PeakGroup(object):
         return representation
 
 
-class Algorithm(object):
+class IntensityClustering(object):
     def __init__(self, peak_file, transformations, rt_thresh=3, tolerance=10):
         self.peaks = []                 # Peaks extracted from a file
         self.peak_file = peak_file      # Data file with M/RT/intensity
@@ -67,13 +67,12 @@ class Algorithm(object):
         self.load_peaks(peak_file)      # Initialize peaks from given file
 
     # Produces a list of peaks from a given file. Numpy, etc. required because of corr_cluster.py code.
-    # Might be avoided if a separate class for peak is created and used instead of borrowing it from corr_cluster.
     def load_peaks(self, file_path):
         print ("\nCreating peaks from a file at: \n" + file_path + "...")
 
         if os.path.isfile(file_path):
             with open(file_path) as f:
-                heading = f.readline()  # Removes heading
+                f.readline()  # Removes heading
                 for line in f:
                     split = line.split(',')
                     peak_id = int(split[0])
@@ -86,92 +85,83 @@ class Algorithm(object):
 
         print ("\n{} peaks loaded!".format(str(len(self.peaks))))
 
-    # Processes and groups the peaks. If we exclude isotopes, takes ~230s to process 7733 peaks.
+    # Processes and groups the peaks around the most intense peaks.
     def heavylifting(self):
         groups = []     # Peak groups that are explained by highest voted m (transformed molecular mass)
 
-        """            1st step of the algorithm           """
         # For testing. m/z: 221.0420, RT: 611.6010,Intensity: 9.84e+05, M+K (182.0789,0.6)
         # most_intense = self.peaks[4316]
+
+        # Sorts the peaks into separate lists, so that we don't need to sort them every time loop starts.
         intensity_sorted = sorted(self.peaks, key=lambda x: x.intensity, reverse=True)  # most-intense to least-intense
         rt_sorted = sorted(self.peaks, key=lambda x: x.rt)                              # lowest rt to highest rt
 
         # Main loop where grouping happens
         while len(intensity_sorted) > 0:
-            """            1st step of the algorithm           """
             most_intense = intensity_sorted[0]  # P:: Always the first one, as it's sorted in desc. order
 
-            """            2nd step of the algorithm           """
             # Creates a set that will contain peaks that are within |n|s RT from most intense peak
             index = rt_sorted.index(most_intense)   # Finds most intense peak's index in rt_sorted list
             thresh_group = []                       # R:: peaks within rt threshold
             thresh_group = self.forward_pass(thresh_group, most_intense, index, rt_sorted)  # Go right (higher)
-            index -= 1
+            index -= 1                              # Immutable, thus 'index' is not changed by the functions.
             thresh_group = self.backward_pass(thresh_group, most_intense, index, rt_sorted) # Go left (lower)
 
-            """            3rd step of the algorithm           """
-            # Finds the possible molecular masses for the most intense peak. Found by applying all transformation to
-            # the most intense peak from the list. As it is the most intense peak, we can assume it is mono-isotopic.
+            # Finds possible molecular masses for the most intense peak by applying all transformations to the most
+            # intense peak from the list. As it is the most intense (abundant) peak, we can assume it is mono-isotopic.
             # Thus, we do not need C13 and 2C13, which we can skip when determining what to add to the molecular masses.
             molecular_masses = []   # M:: list of molecular masses
             for transformation in self.transformations:
                 if transformation.name[-4:-1] != 'C13':
                     molecular_masses.append(transformation.transform(most_intense))
 
-            """            4th step of the algorithm: a           """
             highest_voted = PeakGroup()     # Highest voted group of peaks
 
-            # 1. For each molecular mass, calculates all possible peak masses.
-            # 2. Records the derived mass and the transformation that was used to arrive at that mass.
-            # 3. For each derived mass, checks if such a mass is in a group created around the most intense peak.
-            # 4. If it is, stores it in a group of peaks that are explained by the transformations on M.
+            # 1. For each molecular mass, calculates all possible peak masses. Records it, and the transformation.
+            # 2. For each derived mass, checks if similar mass is in a group created around the most intense peak.
+            # 3. If it is, stores it in a group of peaks that are explained by the transformations on M.
+            # 4. Removes transformations that are missing parents from a group.
+            # 5. Assesses whether the group that was just created has a higher vote than previous highest voted group.
+
             # At least one of the masses must be in that group, as one of the reverse transformations will inevitably
             # arrive at the same mass most intense peak was before transforming it. Other peaks from rt_thresh group
             # might be in there as well. This means that the initial molecule was transformed in different way, and thus
-            # has a different mass when measured. We want to record all such molecules. For this, create an object that
-            # holds total vote (from transformations) and mass, in addition to information on the peak and transforms
-            # that can be used to arrive at the initial mass (or to calculate end mass from initial mass).
+            # has a different mass when measured. To record these molecules, create an object that holds total vote
+            # (from transformations) and mass, in addition to information on the peak and transformation used on it to
+            # arrive at the initial mass (or to calculate end mass from initial mass).
             for mass in molecular_masses:
                 reverse_masses = []   # N:: reverse transformation on molecular masses. Stores mass + transformation
-                # Performs reverse transformations on a given mass. Gives possible masses for the peak.
-                # Since transformations are being done on the "bare" molecular mass, we want to know all possible
-                # values it can take, including C13, 2C13 as well.
+                # Performs reverse transformations on a mass. Gives possible masses for the peak. Includes C13/2C13.
                 for transformation in self.transformations:
                     peak_mass = transformation.reversetransform(mass)
                     reverse_masses.append([peak_mass, transformation])
 
-                """            4th step of the algorithm: c           """
                 # For each peak mass in reverse masses, checks if there is anything similar in rt_thresh group.
                 peak_group = PeakGroup()
                 for peak_mass, transformation in reverse_masses:
                     for peak in thresh_group:
                         # 1000 000 * |(a-b)/a| <= tolerance
-                        ppm = 1000000 * abs((peak.mass - peak_mass) / peak.mass)    # ppm difference
+                        ppm = 1000000 * abs((peak.mass - peak_mass) / peak.mass)    # parts per million (ppm) difference
                         # If there is, adds it to the possible peak group, which will later be assessed on total vote.
                         if ppm <= self.tolerance:
                             peak_group.add_peak(peak, transformation)
-
-                """             Checking for the parent transformations         """
                 peak_group = self.remove_orphans(peak_group)
-
-                """            5th step of the algorithm           """
-                # Only keeps the highest voted group
+                # Only keeps the highest voted group out of all groups around various masses
                 if peak_group.vote > highest_voted.vote:
                     highest_voted = peak_group
 
-            """            6th step of the algorithm           """
-            # Stores the group with the highest vote in a list of groups. Only stores groups that actually have peaks...
+            # Stores the group with the highest vote for the current peak.
             groups.append(highest_voted)
 
-            """            7th step of the algorithm           """
             # Remove peaks that have been grouped (that are explained by m)
             grouped_peaks = [x[0] for x in highest_voted.members]   # List of peaks extracted from a tuple
             for peak in grouped_peaks:
                 rt_sorted.remove(peak)
                 intensity_sorted.remove(peak)
+
+            # Command line: prints how many peaks are left to process. Delete later.
             # print ("{} peaks left to process...".format(len(rt_sorted)))
 
-        """         The End         """
         return groups
 
     """                                 HELPER FUNCTIONS                                    """
@@ -189,10 +179,10 @@ class Algorithm(object):
             for member in to_remove:
                 group.members.remove(member)
 
-            # Enters this only if there were no orphans left in the group
+            # If no orphans were removed, it means every transformation had its parent in a group of transformations.
             if len(to_remove) <= 0:
                 finished = True
-            # Only need to recalculate votes if something was removed
+            # Only need to recalculate mass and votes if something was removed. Otherwise nothing changes.
             else:
                 group.recalculate()
         return group
@@ -219,7 +209,7 @@ class Algorithm(object):
             index -= 1
         return thresh_group
 
-    """                         Temporary: for testing                         """
+    """                         TEMPORARY: for testing                         """
     def print_peaks(self, peak_group=None):
         # Default will print peaks in an order they were retrieved from a file.
         # However, if some parameter is given, this will be skipped and other group of peaks printed.
--- sv_algorithm/sv_main.py
+++ sv_algorithm/sv_main.py
@@ -1,12 +1,16 @@
 import sys, os, time
-from sv_algorithm import Algorithm
-sys.path.insert(0, os.path.normpath(os.getcwd() + os.sep + os.pardir) + '/code/')
+from sv_algorithm import IntensityClustering
+# import cProfile, pstats, StringIO
+sys.path.insert(0, '../code/')
 import transformation
 
+# Profiling stuff
+# pr = cProfile.Profile()
+# pr.enable()
+
 # The path where data files live: currently, it is in grandparent_folder/data/<file>.csv
-# Or in other words, to navigate from the folder where the code is in: >>> cd ../../data.
-# In theory, should work on Unix/Windows, as long as files are in the right folders.
-data_dir = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep + os.pardir) + '/data/'
+grandparent = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep + os.pardir)
+data_dir = grandparent + '/data/'
 data_file = 'Urine_37_fullscan1_POS.csv'
 file_path = data_dir + data_file
 
@@ -20,37 +24,46 @@ transformations = transformation.load_from_file(transformation_path)
 print ("\nLoaded " + str(len(transformations)) + " transformations.")
 
 # Initialises an object that loads peaks from a file and gives the RT threshold in seconds
-cluster = Algorithm(file_path, transformations, rt_thresh=2, tolerance=10)
+cluster = IntensityClustering(file_path, transformations, rt_thresh=2, tolerance=10)
 
 # Taking most intense peak, figuring out the groups around it (within given RT window)
 print ("Processing...")
 start_time = time.clock()
 groups = cluster.heavylifting()
 end_time = time.clock()
-print ("\nData processing time: " + str(end_time-start_time))
+print ("Data processing time: " + str(end_time-start_time))
 
-# Write out the results to a file
-groups = sorted(groups, key=lambda x: x.vote, reverse=True)  # top voted at the top
-output_dir = os.path.normpath(os.getcwd() + os.sep + os.pardir + os.sep + os.pardir) + '/output/'
+# Write out the results to a file. Top voted group presented at the top.
+groups = sorted(groups, key=lambda x: x.vote, reverse=True)
+output_dir = grandparent + '/output/'
 output_file = 'Urine_37_fullscan1_POS_by_vote.txt'
 output_path = output_dir + output_file
 
 with open(output_path, 'w') as f:
     for group in groups:
-        line = "vote: {}, M: {}\n".format(group.vote, group.mass)
+        line = "vote: {}, M: {}\n".format(group.vote, group.M)
         f.write(line)
         line = '\tPeak m/z,Peak rt,Peak intensity,transformation (transformed mass,vote)\n'
         f.write(line)
-        # members = (peak, transformation, transformed_mass); x[1] = transformation
+        # members = (peak, transformation, transformed_mass); x[1] = list(transformation)
         for (peak, transformation, transformed_mass) in sorted(group.members, key=lambda x: x[1].vote, reverse=True):
             line = "\t{:.4f},{:.4f},{:.2e},{} ({:.4f},{:.4f})\n".format(peak.mass, peak.rt, peak.intensity,
                     transformation, transformed_mass, transformation.vote)
             f.write(line)
         f.write('\n')
 
-print_time = time.clock()
-print ("Output file has been prepared! Total execution time: " + str(print_time-start_time))
+print ("\nOutput file has been prepared:\n{}".format(output_path))
 print ("Total groups found: " + str(len(groups)))
 
+# Command line printing
 # for group in groups:
 #     print group
+
+# Profiling continued
+# pr.disable()
+# s = StringIO.StringIO()
+# sortby = 'cumulative'
+# ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+# ps.print_stats()
+# print s.getvalue()
+
